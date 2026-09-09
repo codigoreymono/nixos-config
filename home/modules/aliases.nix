98484d7e849408baaff1c1d9bc9f9644ed9c9907
reymono <adwind007@gmail.com>
2026-09-08 18:33:39 -0600

    "rebuild, aliases"

--- home/modules/aliases.nix
+++ home/modules/aliases.nix
@@ -9,6 +9,9 @@
     rebuild = "sudo nixos-rebuild switch --flake ~/nixos-config#elitebook";
     nixcfg = "cd ~/nixos-config";
 
+    nix-gens = "sudo nix-env --profile /nix/var/nix/profiles/system --list-generations";
+    nix-keep-10 = "sudo nix-env --profile /nix/var/nix/profiles/system --delete-generations +10";
+
     nix-gc = "sudo nix-collect-garbage --delete-older-than 30d";
     nix-optimize = "sudo nix-store --optimise";
     nix-clean = "sudo nix-collect-garbage --delete-older-than 30d && sudo nix-store --optimise";
